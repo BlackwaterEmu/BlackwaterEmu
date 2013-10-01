5cd6bd93968553b2bf2a77258572d4afa1bc07cc
BlackwaterEmu <grammarcore1337@gmail.com>
2013-09-30 17:10:46 -0700

    Those strips of code are no longer needed, found the areatrigger system in the database.

--- Required/scripts/AreaTriggers.vb
+++ Required/scripts/AreaTriggers.vb
@@ -24,23 +24,5 @@ Namespace Scripts
         Public Sub HandleAreaTrigger_4472(ByVal GUID As ULong)
             If CHARACTERs(GUID).HaveAura(34400) = False Then CHARACTERs(GUID).CastOnSelf(34400)
         End Sub
-
-        'Stormwind Stockades
-        Public Sub HandleAreaTrigger_107(ByVal GUID As ULong)
-            If CHARACTERs(GUID).HaveAura(81) = True Then CHARACTERs(GUID).Teleport(-1, 52, -27, 1, 35)
-        End Sub
-        ' Ghostlands
-        Public Sub HandleAreaTrigger_4409(ByVal GUID As ULong)
-            If CHARACTERs(GUID).HaveAura(34400) = False Then CHARACTERs(GUID).CastOnSelf(34400)
-            If CHARACTERs(GUID).HaveAura(34400) = True Then CHARACTERs(GUID).Teleport(7880, -6193, 21, 42, 530)
-
-        End Sub
-
-
-        Public Sub HandleAreaTrigger_4386(ByVal GUID As ULong)
-            If CHARACTERs(GUID).HaveAura(34400) = False Then CHARACTERs(GUID).CastOnSelf(34400)
-            If CHARACTERs(GUID).HaveAura(34400) = True Then CHARACTERs(GUID).Teleport(3468.457, -4482.249, 137.3152, 2.135496, 0)
-
-        End Sub
     End Module
 End Namespace
